2ecde81b4703a2ca2a3dbbd70fe664f8a28979c4
Russell Williams <russellawilliamsjr@gmail.com>
2019-05-26 17:22:06 -0700

    I hate git

--- regextester.py
+++ regextester.py
@@ -1,5 +1,23 @@
+# coding=utf8
+# the above tag defines encoding for this document and is for Python 2.x compatibility
+
 import re
 
-regex = r"[^hl0-9]"
-testStr = "14hl"
-print(re.match(regex, testStr))
+regex = r"[^hl0-9 ]"
+
+test_str = "12 4 lff"
+
+matches = re.finditer(regex, test_str, re.MULTILINE | re.IGNORECASE)
+
+for matchNum, match in enumerate(matches, start=1):
+    
+    print ("Match {matchNum} was found at {start}-{end}: {match}".format(matchNum = matchNum, start = match.start(), end = match.end(), match = match.group()))
+    
+    for groupNum in range(0, len(match.groups())):
+        groupNum = groupNum + 1
+        
+        print ("Group {groupNum} found at {start}-{end}: {group}".format(groupNum = groupNum, start = match.start(groupNum), end = match.end(groupNum), group = match.group(groupNum)))
+
+#regex = r"[^hl0-9]"
+#testStr = "14hl"
+#print(re.match(regex, testStr))
